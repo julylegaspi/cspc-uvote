98e2d97fb5520b11bce2a01c16742c9734733d05
July Palomiano <13739471+julzpalomiano@users.noreply.github.com>
2023-11-27 21:17:24 +0800

    modal position

--- app/Livewire/PositionResource/ListPositions.php
+++ app/Livewire/PositionResource/ListPositions.php
@@ -5,16 +5,55 @@ namespace App\Livewire\PositionResource;
 use Livewire\Component;
 use App\Models\Position;
 use Livewire\WithPagination;
+use Livewire\Attributes\Rule;
 
 class ListPositions extends Component
 {
     use WithPagination;
+
+    public Position $position;
+
+    #[Rule('required|string')]
+    public $name = '';
 
     public $query = '';
 
     public function search()
     {
         $this->resetPage();
+    }
+
+    public function store()
+    {
+        $this->validate();
+        activity()->log("created Position {$this->name}.");
+
+        Position::create([
+            'name' => $this->name
+        ]);
+
+        session()->flash('success', 'Position created.');
+
+        $this->redirect(ListPositions::class);
+    }
+
+    public function edit(Position $position)
+    {
+        $this->position = $position;
+        $this->name = $position->name;
+    }
+
+    public function update()
+    {
+        $this->validate();
+        activity()->log("updated Position.");
+
+        $this->position->name = $this->name;
+        $this->position->save();
+
+        session()->flash('success', 'Position updated.');
+
+        $this->redirect(ListPositions::class);
     }
 
     public function destroy(Position $position)
@@ -25,6 +64,12 @@ class ListPositions extends Component
         session()->flash('success', 'Position deleted.');
 
         $this->redirect(ListPositions::class);
+    }
+
+    public function cancel()
+    {
+        $this->name = "";
+        $this->resetValidation();
     }
     
     public function render()
