87713277b6cabb259063c597d7faec1a73d742c5
July Palomiano <13739471+julzpalomiano@users.noreply.github.com>
2023-11-29 12:20:40 +0800

    added import user modal

--- resources/views/livewire/user-resource/list-users.blade.php
+++ resources/views/livewire/user-resource/list-users.blade.php
@@ -7,10 +7,10 @@
                     class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                     placeholder="Search user" wire:model.live.debounce.150ms="query" wire:keydown='search'>
             </div>
-            <a href="#"
+            <button type="button" data-modal-target="import-user-modal" data-modal-toggle="import-user-modal"
                 class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
                 Import from CSV
-            </a>
+            </button>
         </div>
         <button type="button" data-modal-target="create-modal" data-modal-toggle="create-modal"
             class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
@@ -204,7 +204,8 @@
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                                 <option value="">Select option</option>
                                 @foreach ($sections as $section)
-                                    <option value="{{ $section->id }}">Level {{ $section->level}} - {{ $section->name }}</option>
+                                    <option value="{{ $section->id }}">Level {{ $section->level }} -
+                                        {{ $section->name }}</option>
                                 @endforeach
                             </select>
                             @error('section')
@@ -418,7 +419,8 @@
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                                 <option value="">Select option</option>
                                 @foreach ($sections as $section)
-                                    <option value="{{ $section->id }}">Level {{ $section->level}} - {{ $section->name }}</option>
+                                    <option value="{{ $section->id }}">Level {{ $section->level }} -
+                                        {{ $section->name }}</option>
                                 @endforeach
                             </select>
                             @error('partylist')
@@ -527,4 +529,56 @@
             </div>
         </div>
     </div>
+
+    <!-- Import User -->
+    <div id="import-user-modal" data-modal-backdrop="static" tabindex="-1" aria-hidden="true"
+        class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div class="relative p-4 w-full max-w-2xl max-h-full">
+            <!-- Modal content -->
+            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <!-- Modal header -->
+                <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                    <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                        Import Users
+                    </h3>
+                    <button type="button"
+                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                        data-modal-hide="import-user-modal">
+                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 14 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                        </svg>
+                        <span class="sr-only">Close modal</span>
+                    </button>
+                </div>
+                <!-- Modal body -->
+                <div class="p-4 md:p-5 space-y-4">
+                    <p>
+                        1. Download the <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Template.</a>
+                    </p>
+                    <p> 
+                        2. Fill up the correct fields.
+                    </p>
+                    <p>
+                        3. Import
+                    </p>
+                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                        for="file_input">Select CSV*</label>
+                    <input
+                        class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                        aria-describedby="file_input_help" id="file_input" type="file">
+
+                </div>
+                <!-- Modal footer -->
+                <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                    <button data-modal-hide="import-user-modal" type="button"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Import</button>
+                    <button data-modal-hide="import-user-modal" type="button"
+                        class="ml-2 ms-3 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Cancel</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
